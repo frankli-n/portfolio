ea5c066ca84231b105181ff15c00d5303d276b82
Frankie <frankiehaubrich@gmail.com>
2023-04-07 15:18:02 +1000

    a

--- hide.js
+++ hide.js
@@ -1,7 +1,15 @@
 const asterix = document.getElementById("asterix")
 const projects = document.getElementById("project-section")
+const birds = document.getElementById("birds")
 
-asterix.addEventListener("click", function(){
+const birdList = []
+//     corellaPie:`<h2>birds:</h2><img id="birds" src="crap-birds/corella-pie.jpg" alt="imagine a bird"></img>`,
+//     jumpers:`<h2>birds:</h2><img id="birds" src="crap-birds/jumpers.jpg" alt="imagine a bird"></img>`,
+//     cockatoos:`<h2>birds:</h2><img id="birds" src="crap-birds/cockatoos-cards.jpg" alt="imagine a bird"></img>`
+//     }
+
+
+asterix.addEventListener("click", function () {
     asterix.style.display = "none"
     projects.style.display = "block"
 
@@ -14,5 +22,28 @@ asterix.addEventListener("click", function(){
     document.getElementById("i").style.color = "#5b2397";
     document.getElementById("dash").style.color = "#239597";
     document.getElementById("n2").style.color = "972395";
-    } 
+}
 )
+
+birds.addEventListener("click", function () {
+    birdsFunction()
+})
+
+function birdsFunction() {
+    let iBird = document.getElementById("iBird").src
+
+    // birds.innerHTML = `<h2>birds:</h2><img id="birds" src="crap-birds/jumpers.jpg" alt="imagine a bird"></img>`
+    console.log(iBird)
+    if (iBird === "http://127.0.0.1:5500/crap-birds/corella-pie.jpg") {
+        birds.innerHTML = `<h2>birds:</h2><img id="iBird" src="crap-birds/jumpers.jpg" alt="imagine a bird"></img>`
+        iBird = document.getElementById("iBird").src
+    }
+    else if (iBird === "http://127.0.0.1:5500/crap-birds/jumpers.jpg") {
+        birds.innerHTML =`<h2>birds:</h2><img id="iBird" src="crap-birds/cockatoos-cards.jpg" alt="imagine a bird"></img>`
+        iBird = document.getElementById("iBird").src
+    }
+    else if (iBird === "http://127.0.0.1:5500/crap-birds/cockatoos-cards.jpg") {
+        birds.innerHTML = `<h2>birds:</h2><img id="iBird" src="crap-birds/corella-pie.jpg" alt="imagine a bird"></img>`
+        iBird = document.getElementById("iBird").src
+    }
+}
