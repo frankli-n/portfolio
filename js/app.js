4559dd2f2b600edd3f16e06be1b39b471989e303
Frankie <frankiehaubrich@gmail.com>
2023-05-31 14:34:13 +1000

    a

--- js/app.js
+++ js/app.js
@@ -99,15 +99,6 @@ function changeImageHover() {
     });
 }
 
-
-
-
-    
-
-
-
-
-
 function isTouchDevice() {
     return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
   }
